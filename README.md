504d0e3e219f85bb32b6deae9aa5e7361edbabf8
Ryan Closner <ryan@ryanclosner.com>
2012-12-10 18:38:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Strapping
+
+## Overview
+
+Strapping is a template application for new projects.
+
+## Features
+
+* Twitter Bootstrap SASS
+* HTML5 Boilerplate
+* HAML
+
+## Installation
+
+* Clone the git repository
+* Change the app name
+* Go to Work
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strapping::Application.config.secret_token = 'd8e23c3d1c6c3338fd92830198dc164ad616c95fde840168e1edc0bda06ce280e965320ab3f54e1984f857671ec219e23aa974df14f1f70cb57e61671eb78153'
